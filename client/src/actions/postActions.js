4e9a46c08440fdd44c038c7f28616ff2f267fe4a
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-10-07 00:22:40 -0400

    Rename to postActions, remove saga service

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
-import { FETCH_POSTS_SUCCESS, FETCH_POSTS_FAIL, SEARCH_POSTS, FETCHING_POSTS } from '../actions/types';
+import { FETCH_POSTS_SUCCESS, FETCH_POSTS_FAIL, SEARCH_POSTS, FETCHING_POSTS } from './types';
 import { postService } from '../services/post';
+import axios from 'axios';
 
 export const searchPosts = (search_filter, easy_filters, userId) => ({
     type: SEARCH_POSTS,
@@ -10,15 +11,18 @@ export const searchPosts = (search_filter, easy_filters, userId) => ({
 });
 
 // Fetch posts based on filter or interest
-export const fetchPosts = (search_filter, easy_filters, userId) => async dispatch => {
+export const fetchPosts = (search_filter, easy_filters, userId, size) => async dispatch => {
     try {
         dispatch({
             type: FETCHING_POSTS
         });
-        const res = await postService.searchPosts({ search_filter, easy_filters, userId });
+        // const res = await postService.searchPosts({ search_filter, easy_filters, userId });
+        const res = await axios.get(
+            `/posts?userId=${userId}&search_filter=${search_filter}&easy_filters=${easy_filters}&size=${size}`
+        );
         dispatch({
             type: FETCH_POSTS_SUCCESS,
-            payload: res
+            payload: res.data
         });
     } catch (err) {
         dispatch({
