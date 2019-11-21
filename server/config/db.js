c0f283750072ba8a4ade77b03ce2023332a8b84d
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-11-20 20:41:42 -0500

    Uses mongo atlas

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const db = process.env.MONGO_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.log(err.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
