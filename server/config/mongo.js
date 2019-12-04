4a5708ddd1fb1451432c18687e6c934a3e5bd88e
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-12-04 17:48:46 -0500

    mongo uri error

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -5,7 +5,7 @@ let uri = '';
 if(process.env.NODE_ENV === 'dev') {
     uri = process.env.MONGO_URI;
 } else if(process.env.NODE_ENV === 'prod') {
-    uri = process.env.MONGODB_URI;
+    uri = process.env.MONGO_URI;
 } else {
     uri = process.env.MONGO_URI + '/test';
 }
