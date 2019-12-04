42bc178785182cc6d0026c713743bd4369941b8f
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-12-04 16:56:08 -0500

    fix

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const db = process.env.MONGO_URI;
-const uri = process.env.NODE_ENV === 'dev' ? process.env.MONGO_URI + '/pineapple' : process.env.MONGO_URI;
+let uri = '';
+if(process.env.NODE_ENV === 'dev') {
+    uri = process.env.MONGO_URI;
+} else if(process.env.NODE_ENV === 'prod') {
+    uri = process.env.MONGODB_URI;
+} else {
+    uri = process.env.MONGO_URI + '/test';
+}
 
-if(!process.env.MONGO_URI) {
+if(!uri) {
     throw new Error('No Mongo URI provided in environment');
 } else {
-    mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+    mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
     // eslint-disable-next-line no-console
         .then(() => console.log('MongoDB successfully connected'))
         // eslint-disable-next-line no-console
