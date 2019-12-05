5910a313a2701739b2d665d68ddd9410e00aa851
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-12-04 23:57:49 -0500

    Changed prod to production

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-if(process.env.NODE_ENV === 'prod') {
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
