835e3ed2b059f1f9b490bc0d42ec6a9fd325ad54
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-12-05 01:08:34 -0500

    Heroku

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(expressValidator());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', express.static(path.join(__dirname, '../client/build')));
+app.use('/', indexRouter);
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
--- server/config/mongo.js
+++ server/config/mongo.js
@@ -3,11 +3,11 @@ mongoose.Promise = global.Promise;
 
 let uri = '';
 if(process.env.NODE_ENV === 'dev') {
-    uri = process.env.MONGODB_URI;
-} else if(process.env.NODE_ENV === 'prod') {
+    uri = process.env.MONGO_URI + '/pineapple';
+} else if(process.env.NODE_ENV === 'production') {
     uri = process.env.MONGODB_URI;
 } else {
-    uri = process.env.MONGODB_URI + '/test';
+    uri = process.env.MONGO_URI + '/test';
 }
 
 if(!uri) {
